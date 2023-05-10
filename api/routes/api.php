d8b950b9b2f4af1fcd16005298f0c147c033946d
martiniz <martiniz1901@gmail.com>
2023-05-10 11:19:45 +0300

    Modification Docker-compose file. Nginx 80:80

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
     Route::get('/events', [EventController::class, 'show'])->name('getAllEvents');
     Route::get('/events/{id}/hall', [BlockController::class, 'show'])->name('getEventById');
     Route::post('/order', [OrderController::class, 'create'])->name('createOrder');
-    Route::get('/order/{order}/confirm', [OrderController::class, 'confirm'])->name('confirmOrderById');
+    Route::post('/order/{order}/confirm', [OrderController::class, 'confirm'])->name('confirmOrderById');
     Route::post('/order/{order}/cancel', [OrderController::class, 'cancel'])->name('cancelOrderById');
     Route::get('/order/{order}', [OrderController::class, 'get'])->name('getOrderById');
 
